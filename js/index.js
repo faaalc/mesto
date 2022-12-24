257ae34992b31e4927247080405b6fd2fd6dbf3f
Danil Nepomnyashchiy <ya.idioma@yandex.ru>
2022-12-25 02:00:11 +0300

    пофиксить модалку

--- js/index.js
+++ js/index.js
@@ -12,7 +12,6 @@ const popupEdit = {
 
 const open = () => {
   popupEdit.popup.classList.add('popup_opened')
-  popupEdit.root.style.overflow = 'hidden'
   popupEdit.nameInput.value = popupEdit.profileName.textContent
   popupEdit.descriptionInput.value = popupEdit.profileDescription.textContent
 }
@@ -26,7 +25,6 @@ const save = e => {
   popupEdit.profileName.textContent = popupEdit.nameInput.value
   popupEdit.profileDescription.textContent = popupEdit.descriptionInput.value
   popupEdit.popup.classList.remove('popup_opened')
-  popupEdit.root.style.overflow = 'auto'
 }
 
 popupEdit.openBtn.addEventListener('click', open)
