d2d633c8e1fe50daaa78aaedf4e80d96e2dcd59a
Danil Nepomnyashchiy <ya.idioma@yandex.ru>
2022-12-27 16:40:59 +0300

    hotfix/ исправить ошибки

--- js/index.js
+++ js/index.js
@@ -1,30 +1,29 @@
 const popupEdit = {
   popup: document.querySelector('.popup'),
+  form: document.querySelector('.popup__form_edit-description'),
   openBtn: document.querySelector('.profile__edit-button'),
   closeBtn: document.querySelector('.popup__close'),
-  saveBtn: document.querySelector('.popup__save'),
-  nameInput: document.querySelector('input[name="popup-name"]'),
-  descriptionInput: document.querySelector('input[name="popup-description"]'),
+  nameInput: document.querySelector('.popup__input_name'),
+  descriptionInput: document.querySelector('.popup__input_description'),
   profileName: document.querySelector('.profile__name'),
   profileDescription: document.querySelector('.profile__description'),
 }
-
-const open = () => {
+//Действия кнопок
+const openPopup = () => {
   popupEdit.popup.classList.add('popup_opened')
   popupEdit.nameInput.value = popupEdit.profileName.textContent
   popupEdit.descriptionInput.value = popupEdit.profileDescription.textContent
 }
-const close = e => {
-  e.preventDefault()
+const closePopup = () => {
   popupEdit.popup.classList.remove('popup_opened')
 }
-const save = e => {
+const saveChanges = e => {
   e.preventDefault()
   popupEdit.profileName.textContent = popupEdit.nameInput.value
   popupEdit.profileDescription.textContent = popupEdit.descriptionInput.value
-  popupEdit.popup.classList.remove('popup_opened')
+  closePopup()
 }
 
-popupEdit.openBtn.addEventListener('click', open)
-popupEdit.closeBtn.addEventListener('click', close)
-popupEdit.saveBtn.addEventListener('click', save)
+popupEdit.openBtn.addEventListener('click', openPopup)
+popupEdit.closeBtn.addEventListener('click', closePopup)
+popupEdit.form.addEventListener('submit', saveChanges)
