2f67b218d4006cfb7799adde49733e5cf745dd97
Danil Nepomnyashchiy <ya.idioma@yandex.ru>
2023-02-05 22:58:04 +0300

    рефактор функции генерации модалки

--- js/index.js
+++ js/index.js
@@ -1,92 +1,138 @@
-//Popup functions
-const closePopup = (popup) => popup.classList.remove('popup_opened')
-const openPopup = (popup) => popup.classList.add('popup_opened')
-/**
- * Generates HTML popup element.
- *
- * @param {Object} settings Settings
- * @param {string[]} settings.className Popup classes
- * @param {string} settings.title Popup title
- * @param {Object} settings.form /////Popup form/////
- * @param {string[]} settings.form.className Popup form classes
- * @param {string} settings.form.name Popup name
- * @param {function} settings.form.callback Popup submit function
- * @param {Object} settings.firstInput /////Popup 1st input/////
- * @param {string[]} settings.firstInput.className Input classes
- * @param {string} settings.firstInput.name Input name
- * @param {string} settings.firstInput.type Input type
- * @param {string} settings.firstInput.placeholder Input placeholder
- * @param {Object} settings.secondInput /////Popup 2nd input/////
- * @param {string[]} settings.secondInput.className Input classes
- * @param {string} settings.secondInput.name Input name
- * @param {string} settings.secondInput.type Input type
- * @param {string} settings.secondInput.placeholder Input placeholder
- * @param {Object} settings.button /////Popup save button/////
- * @param {string} settings.button.title Button title
- * @param {string} settings.button.ariaLabel Button aria-label
- * @return {HTMLElement} HTML popup element.
- */
-const generatePopup = (settings) => {
-  const {className, title, form, firstInput, secondInput, button} = settings,
-    popup = document.createElement('div')
-  popup.classList.add('popup', className.join((' ')))
+document.addEventListener('DOMContentLoaded', () => {
+  const GALLERY = document.querySelector('.gallery'),
+    profileOpenButton = document.querySelector('.profile__edit-button'),
+    profileAddButton = document.querySelector('.profile__add-button')
 
-  popup.innerHTML = `
-    <div class="popup__container">
-      <h2 class="popup__title">${title}</h2>
-      <form method="post" action="#" name="${form.name}" class="popup__form ${form.className.join(' ')}">
-        <input required 
-          type="${firstInput.type}" 
-          class="popup__input ${firstInput.className.join(' ')}" 
-          name="${firstInput.name}" 
-          placeholder="${firstInput.placeholder}">
-        <input required 
-          type="${secondInput.type}" 
-          class="popup__input ${secondInput.className.join(' ')}" 
-          name="${secondInput.name}" 
-          placeholder="${secondInput.placeholder}">
-        <button class="button popup__save" type="submit" aria-label="${button.ariaLabel}">${button.title}</button>
-      </form>
-      <button class="button popup__close" type="button" aria-label="Закрыть модальное окно"></button>
-    </div>
-  `
 
-  //adding listeners
-  const popupButtonClose = popup.querySelector('.popup__close'),
-    popupForm = popup.querySelector('.popup__form')
+//Custom popup generator
+  /**
+   * Generates HTML popup element.
+   *
+   * @param {Object} settings Settings
+   * @param {string[]} settings.className Popup classes
+   * @param {string} [settings.title] Popup title
+   * @param {Object} [settings.form] /////Popup form/////
+   * @param {string[]} settings.form.className Popup form classes
+   * @param {string} settings.form.name Popup name
+   * @param {function} settings.form.callback Popup submit function
+   * @param {Object} settings.form.firstInput /////Popup 1st input/////
+   * @param {string[]} settings.form.firstInput.className Input classes
+   * @param {string} settings.form.firstInput.name Input name
+   * @param {string} settings.form.firstInput.type Input type
+   * @param {string} settings.form.firstInput.placeholder Input placeholder
+   * @param {Object} [settings.form.secondInput] /////Popup 2nd input/////
+   * @param {string[]} settings.form.secondInput.className Input classes
+   * @param {string} settings.form.secondInput.name Input name
+   * @param {string} settings.form.secondInput.type Input type
+   * @param {string} settings.form.secondInput.placeholder Input placeholder
+   * @param {Object} settings.form.button /////Popup save button/////
+   * @param {string} settings.form.button.title Button title
+   * @param {string} settings.form.button.ariaLabel Button aria-label
+   * @param {Object} [settings.image] /////Popup for images/////
+   * @param {string} settings.image.img Image link
+   * @param {string} settings.image.location Image location
+   * @return {HTMLElement} HTML popup element.
+   */
+  const generatePopup = (settings) => {
+    const {className, title, form, image} = settings,
+      popup = document.createElement('div'),
+      container = document.createElement('div'),
+      closePopup = (popup) => popup.remove()
 
-  popupButtonClose.addEventListener('click', () => closePopup(popup))
-  popupForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const formData = new FormData(e.target),
-          formValues = Object.values(Object.fromEntries(formData))
-    if (formValues.every(v => !!v.trim())) {
-      form.callback()
-      closePopup(popup)
+    popup.classList.add('popup', ...className)
+    container.classList.add('popup__container')
+
+    //Optional title
+    if (title) {
+      const titleHTML = `<h2 class="popup__title">${title}</h2>`
+      container.insertAdjacentHTML('beforeend', titleHTML)
     }
-  })
 
-  document.body.append(popup)
-  return popup
-}
+    //Optional form
+    if (form) {
+      const {firstInput, secondInput, button} = form
+      const formHTML = `<form method="post" action="#" 
+            name="${form.name}" 
+            class="popup__form ${form.className.join(' ')}"></form>`
+      container.insertAdjacentHTML('beforeend', formHTML)
+      const formEl = container.querySelector('form')
+
+      //Required input
+      const firstInputHTML = `<input required
+          type="${firstInput.type}"
+          class="popup__input ${firstInput.className.join(' ')}"
+          name="${firstInput.name}"
+          placeholder="${firstInput.placeholder}">`
+      formEl.insertAdjacentHTML('beforeend', firstInputHTML)
+
+      // Optional input
+      if (secondInput) {
+        const secondInputHTML = `<input required
+            type="${secondInput.type}"
+            class="popup__input ${secondInput.className.join(' ')}"
+            name="${secondInput.name}"
+            placeholder="${secondInput.placeholder}">`
+        formEl.insertAdjacentHTML('beforeend', secondInputHTML)
+      }
+
+      // Required button
+      const buttonHTML = `<button 
+          class="button popup__save" 
+          type="submit" 
+          aria-label="${button.ariaLabel}">${button.title}</button>`
+      formEl.insertAdjacentHTML('beforeend', buttonHTML)
+
+      //Adding listener and validation
+      formEl.addEventListener('submit', e => {
+        e.preventDefault()
+        const formData = new FormData(e.target),
+          formValues = Object.values(Object.fromEntries(formData))
+        if (formValues.every(v => !!v.trim())) {
+          form.callback()
+          closePopup(popup)
+        }
+      })
+    }
+
+    //Optional (image popup)
+    if (image) {
+      const {img, location} = image
+      container.classList.add('popup-image__container')
+      const html = `<img class="popup-image__image" src="${img}" alt="${img}">
+                  <p class="popup-image__location">${location}</p>`
+      container.insertAdjacentHTML('beforeend', html)
+    }
+
+    //Close button
+    const closeButtonHTML = `<button class="button popup__close" type="button" aria-label="Закрыть модальное окно"></button>`
+    container.insertAdjacentHTML('beforeend', closeButtonHTML)
+
+    const closeButtonEl = container.querySelector('.popup__close')
+    closeButtonEl.addEventListener('click', () => closePopup(popup))
+
+
+    popup.append(container)
+    document.body.append(popup)
+    return popup
+  }
 
 
 //Gallery
-const GALLERY = document.querySelector('.gallery')
-/**
- * Generates and prepends HTML card element.
- *
- * @param {Object} data Information about the card
- * @param {string} data.name Location
- * @param {string} data.link Image link
- * @param {HTMLElement} selector HTML selector to add the card
- */
-const generateAndAddCard = (data, selector) => {
-  const {name, link} = data,
-    card = document.createElement('div')
-  card.classList.add('card')
+  /**
+   * Generates and prepends HTML card element.
+   *
+   * @param {Object} data Information about the card
+   * @param {string} data.name Location
+   * @param {string} data.link Image link
+   * @param {HTMLElement} selector HTML selector to add the card
+   */
+  const generateAndAddCard = (data, selector) => {
+    const {name, link} = data,
+      card = document.createElement('div')
+    card.classList.add('card')
 
-  card.innerHTML = `
+    //Creating card
+    card.innerHTML = `
     <img src=${link} alt="${name}" class="card__photo">
     <div class="card__description">
       <h2 class="card__location">${name}</h2>
@@ -95,134 +141,122 @@ const generateAndAddCard = (data, selector) => {
     <button class="button card__delete-button" type="button" aria-label="Удалить пост"></button>
   `
 
-  //adding listeners
-  const likeButton = card.querySelector('.card__like-button'),
-    deleteButton = card.querySelector('.card__delete-button'),
-    likeCard = () => likeButton.classList.toggle('card__like-button_active'),
-    deleteCard = () => deleteButton.closest('.card').remove(),
-    openCard = e => {
-      const image = e.target.src,
-        location = e.target.alt,
-        popup = document.createElement('div')
-      popup.classList.add('popup', 'popup-image', 'popup_opened')
+    //Creating functions for listeners
+    const likeButton = card.querySelector('.card__like-button'),
+      deleteButton = card.querySelector('.card__delete-button'),
+      likeCard = () => likeButton.classList.toggle('card__like-button_active'),
+      deleteCard = () => deleteButton.closest('.card').remove(),
+      openCard = (e) => {
+        generatePopup({
+          className: ['popup-image'],
+          image: {
+            img: e.target.src,
+            location: e.target.alt
+          }
+        })
+      }
 
-      popup.innerHTML = `
-            <div class="popup-image__container">
-              <img class="popup-image__image" src="${image}" alt="${image}">
-              <p class="popup-image__location">${location}</p>
-              <button class="button popup__close" type="button" aria-label="Закрыть модальное окно"></button>
-            </div>
-          `
+    //Adding listeners
+    card.addEventListener('click', e => {
+      const checkTarget = (e, className) => e.target.classList.contains(className)
+      checkTarget(e, 'card__like-button') && likeCard()
+      checkTarget(e, 'card__delete-button') && deleteCard()
+      checkTarget(e, 'card__photo') && openCard(e)
+    })
 
-      const popupButtonClose = popup.querySelector('.popup__close')
-      popupButtonClose.addEventListener('click', e => {
-        e.target.closest('.popup').remove()
-      })
-      document.body.append(popup)
-    }
+    selector.prepend(card)
+  }
 
-  card.addEventListener('click', e => {
-    const checkTarget = (e, className) => e.target.classList.contains(className)
-    checkTarget(e, 'card__like-button') && likeCard()
-    checkTarget(e, 'card__delete-button') && deleteCard()
-    checkTarget(e, 'card__photo') && openCard(e)
-  })
-
-
-  selector.prepend(card)
-}
-
-const addCardsToGallery = (cards, selector) => {
-  cards.forEach(card => generateAndAddCard(card, selector))
-}
-addCardsToGallery(initialCards, GALLERY)
+  //Adding cards from data to page
+  (function () {
+    initialCards.forEach(card => generateAndAddCard(card, GALLERY))
+  })()
 
 
 //Creating and configurating edit popup
-const popupEdit = generatePopup({
-    className: ['popup_type_edit'],
-    title: 'Редактировать профиль',
-    form: {
-      className: ['popup__form_edit-description'],
-      name: 'edit-description',
-      callback: saveChangesInEditPopup
-    },
-    firstInput: {
-      className: ['popup__input_type_name'],
-      name: 'popup-name',
-      type: 'text',
-      placeholder: 'Имя'
-    },
-    secondInput: {
-      className: ['popup__input_type_description'],
-      name: 'popup-description',
-      type: 'text',
-      placeholder: 'О себе'
-    },
-    button: {
-      title: 'Сохранить',
-      ariaLabel: 'Сохранить изменения'
+  const openEditPopup = () => {
+    const popupEdit = generatePopup({
+        className: ['popup_type_edit'],
+        title: 'Редактировать профиль',
+        form: {
+          className: ['popup__form_edit-description'],
+          name: 'edit-description',
+          callback: saveChangesInEditPopup,
+          firstInput: {
+            className: ['popup__input_type_name'],
+            name: 'popup-name',
+            type: 'text',
+            placeholder: 'Имя'
+          },
+          secondInput: {
+            className: ['popup__input_type_description'],
+            name: 'popup-description',
+            type: 'text',
+            placeholder: 'О себе'
+          },
+          button: {
+            title: 'Сохранить',
+            ariaLabel: 'Сохранить изменения'
+          }
+        }
+      }),
+      popupEditNameInput = popupEdit.querySelector('.popup__input_type_name'),
+      popupEditDescriptionInput = popupEdit.querySelector('.popup__input_type_description'),
+      profileName = document.querySelector('.profile__name'),
+      profileDescription = document.querySelector('.profile__description')
+
+    //Setting profile info into inputs
+    popupEditNameInput.value = profileName.textContent
+    popupEditDescriptionInput.value = profileDescription.textContent
+
+    function saveChangesInEditPopup() {
+      profileName.textContent = popupEditNameInput.value
+      profileDescription.textContent = popupEditDescriptionInput.value
     }
-  }),
-  popupEditNameInput = popupEdit.querySelector('.popup__input_type_name'),
-  popupEditDescriptionInput = popupEdit.querySelector('.popup__input_type_description'),
-  profileName = document.querySelector('.profile__name'),
-  profileDescription = document.querySelector('.profile__description'),
-  profileOpenButton = document.querySelector('.profile__edit-button')
-
-const openEditPopup = () => {
-  openPopup(popupEdit)
-  popupEditNameInput.value = profileName.textContent
-  popupEditDescriptionInput.value = profileDescription.textContent
-}
-
-function saveChangesInEditPopup() {
-  profileName.textContent = popupEditNameInput.value
-  profileDescription.textContent = popupEditDescriptionInput.value
-}
-
-profileOpenButton.addEventListener('click', openEditPopup)
-
-
-//Creating and configurating add post popup
-const popupAdd = generatePopup({
-    className: ['popup_type_add'],
-    title: 'Новое место',
-    form: {
-      className: ['popup__form_add-card'],
-      name: 'add-card',
-      callback: addNewCardFromPopup
-    },
-    firstInput: {
-      className: ['popup__input_type_place'],
-      name: 'popup-place',
-      type: 'text',
-      placeholder: 'Место'
-    },
-    secondInput: {
-      className: ['popup__input_type_link'],
-      name: 'popup-link',
-      type: 'url',
-      placeholder: 'Ссылка на картинку'
-    },
-    button: {
-      title: 'Создать',
-      ariaLabel: 'Создать карточку'
-    }
-  }),
-  popupAddPlaceInput = popupAdd.querySelector('.popup__input_type_place'),
-  popupAddLinkInput = popupAdd.querySelector('.popup__input_type_link'),
-  profileAddButton = document.querySelector('.profile__add-button')
-
-
-function addNewCardFromPopup() {
-  const data = {
-    name: popupAddPlaceInput.value,
-    link: popupAddLinkInput.value
   }
-  generateAndAddCard(data, GALLERY)
-  popupAddPlaceInput.value = ''
-  popupAddLinkInput.value = ''
-}
+  profileOpenButton.addEventListener('click', openEditPopup)
 
-profileAddButton.addEventListener('click', () => openPopup(popupAdd))
+
+// Creating and configurating add post popup
+  const openAddPopup = () => {
+    const popupAdd = generatePopup({
+        className: ['popup_type_add'],
+        title: 'Новое место',
+        form: {
+          className: ['popup__form_add-card'],
+          name: 'add-card',
+          callback: addNewCardFromPopup,
+          firstInput: {
+            className: ['popup__input_type_place'],
+            name: 'popup-place',
+            type: 'text',
+            placeholder: 'Место'
+          },
+          secondInput: {
+            className: ['popup__input_type_link'],
+            name: 'popup-link',
+            type: 'url',
+            placeholder: 'Ссылка на картинку'
+          },
+          button: {
+            title: 'Создать',
+            ariaLabel: 'Создать карточку'
+          }
+        }
+      }),
+      popupAddPlaceInput = popupAdd.querySelector('.popup__input_type_place'),
+      popupAddLinkInput = popupAdd.querySelector('.popup__input_type_link')
+
+
+    function addNewCardFromPopup() {
+      const data = {
+        name: popupAddPlaceInput.value,
+        link: popupAddLinkInput.value
+      }
+      generateAndAddCard(data, GALLERY)
+      popupAddPlaceInput.value = ''
+      popupAddLinkInput.value = ''
+    }
+  }
+  profileAddButton.addEventListener('click', openAddPopup)
+})
